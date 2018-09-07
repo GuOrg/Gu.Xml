68a7942d885acb829bc9a70ecf97e1fb7a5b9214
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-07 18:45:02 +0200

    Benchmarks WIP

---
+++ Gu.Xml.Benchmarks/MemoryDiagnoserConfig.cs
@@ -0,0 +1,14 @@
+﻿[assembly: BenchmarkDotNet.Attributes.Config(typeof(Gu.Xml.Benchmarks.MemoryDiagnoserConfig))]
+namespace Gu.Xml.Benchmarks
+{
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Diagnosers;
+
+    public class MemoryDiagnoserConfig : ManualConfig
+    {
+        public MemoryDiagnoserConfig()
+        {
+            this.Add(MemoryDiagnoser.Default);
+        }
+    }
+}
---
+++ Gu.Xml.Benchmarks/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Gu.Xml.Benchmarks
+{
+    using System;
+
+    public class Program
+    {
+        //public static void Main()
+        //{
+        //    foreach (var summary in RunSingle<FindVsFirstOrDefault>())
+        //    {
+        //        CopyResult(summary);
+        //    }
+        //}
+
+        //// ReSharper disable once UnusedMember.Local
+        //private static IEnumerable<Summary> RunAll()
+        //{
+        //    var switcher = new BenchmarkSwitcher(typeof(Program).Assembly);
+        //    var summaries = switcher.RunAll();
+        //    return summaries;
+        //}
+
+        //private static IEnumerable<Summary> RunSingle<T>()
+        //{
+        //    var summaries = new[] { BenchmarkRunner.Run<T>() };
+        //    return summaries;
+        //}
+
+        //private static void CopyResult(Summary summary)
+        //{
+        //    var sourceFileName = Directory.EnumerateFiles(summary.ResultsDirectoryPath, $"*{summary.Title}-report-github.md")
+        //                                  .Single();
+        //    var destinationFileName = Path.Combine(summary.ResultsDirectoryPath, "..\\..\\..\\..\\..\\", summary.Title + ".md");
+        //    Console.WriteLine($"Copy: {sourceFileName} -> {destinationFileName}");
+        //    File.Copy(sourceFileName, destinationFileName, overwrite: true);
+        //}
+    }
+}
