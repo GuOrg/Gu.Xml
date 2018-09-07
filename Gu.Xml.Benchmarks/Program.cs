630498ea9d74075f6738ac9199c8e8f726f44ddb
Johan Larsson <johan_gavle1@hotmail.com>
2018-09-07 22:48:07 +0200

    Fix Program.cs

--- Gu.Xml.Benchmarks/Program.cs
+++ Gu.Xml.Benchmarks/Program.cs
@@ -1,38 +1,42 @@
 ﻿namespace Gu.Xml.Benchmarks
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using BenchmarkDotNet.Reports;
+    using BenchmarkDotNet.Running;
 
     public class Program
     {
-        //public static void Main()
-        //{
-        //    foreach (var summary in RunSingle<FindVsFirstOrDefault>())
-        //    {
-        //        CopyResult(summary);
-        //    }
-        //}
+        public static void Main()
+        {
+            foreach (var summary in RunAll())
+            {
+                CopyResult(summary);
+            }
+        }
 
-        //// ReSharper disable once UnusedMember.Local
-        //private static IEnumerable<Summary> RunAll()
-        //{
-        //    var switcher = new BenchmarkSwitcher(typeof(Program).Assembly);
-        //    var summaries = switcher.RunAll();
-        //    return summaries;
-        //}
+        private static IEnumerable<Summary> RunAll()
+        {
+            var switcher = new BenchmarkSwitcher(typeof(Program).Assembly);
+            var summaries = switcher.RunAll();
+            return summaries;
+        }
 
-        //private static IEnumerable<Summary> RunSingle<T>()
-        //{
-        //    var summaries = new[] { BenchmarkRunner.Run<T>() };
-        //    return summaries;
-        //}
+        private static IEnumerable<Summary> RunSingle<T>()
+        {
+            var summaries = new[] { BenchmarkRunner.Run<T>() };
+            return summaries;
+        }
 
-        //private static void CopyResult(Summary summary)
-        //{
-        //    var sourceFileName = Directory.EnumerateFiles(summary.ResultsDirectoryPath, $"*{summary.Title}-report-github.md")
-        //                                  .Single();
-        //    var destinationFileName = Path.Combine(summary.ResultsDirectoryPath, "..\\..\\..\\..\\..\\", summary.Title + ".md");
-        //    Console.WriteLine($"Copy: {sourceFileName} -> {destinationFileName}");
-        //    File.Copy(sourceFileName, destinationFileName, overwrite: true);
-        //}
+        private static void CopyResult(Summary summary)
+        {
+            var sourceFileName = Directory.EnumerateFiles(summary.ResultsDirectoryPath, $"*{summary.Title}-report-github.md")
+                                          .Single();
+            var destinationFileName = Path.Combine(summary.ResultsDirectoryPath, "..\\..\\..\\..\\..\\", summary.Title.Split('.').Last() + ".md");
+            Console.WriteLine($"Copy: {sourceFileName} -> {destinationFileName}");
+            File.Copy(sourceFileName, destinationFileName, overwrite: true);
+        }
     }
 }
