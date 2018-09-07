85c40bb49bb0acd380e35c9c8f0fc97361d18f90
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-07 18:27:15 +0200

    Test project.

---
+++ Gu.Xml.Tests/Remove.cs
@@ -0,0 +1,13 @@
+﻿namespace Gu.Xml.Tests
+{
+    using NUnit.Framework;
+
+    public class Remove
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
