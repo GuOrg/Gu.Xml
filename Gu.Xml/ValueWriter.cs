7f3b047643d145c17ab83b0ddf4c5528cec7c1d3
Johan Larsson <johan_gavle1@hotmail.com>
2018-09-09 00:13:11 +0200

    Refactor cleanup ValueWriter

--- Gu.Xml/ValueWriter.cs
+++ Gu.Xml/ValueWriter.cs
@@ -4,15 +4,25 @@
     using System.Collections.Generic;
     using System.Globalization;
     using System.IO;
-    using System.Linq;
 
     public abstract class ValueWriter
     {
-        private static readonly Dictionary<Type, ValueWriter> SimpleValueWriters = CreateWriters(
-            Create<bool>((writer, value) => writer.Write(value ? "true" : "false")),
-            Create<bool?>((writer, value) => writer.Write(value.Value ? "true" : "false")),
-            Create<int>((writer, value) => writer.Write(value.ToString(NumberFormatInfo.InvariantInfo))),
-            Create<double>(WriteDouble));
+        private static readonly Dictionary<Type, ValueWriter> SimpleValueWriters = new Dictionary<Type, ValueWriter>();
+
+        static ValueWriter()
+        {
+            Add<bool>((writer, value) => writer.Write(value ? "true" : "false"));
+            Add<int>((writer, value) => writer.Write(value.ToString(NumberFormatInfo.InvariantInfo)));
+            Add<double>(WriteDouble);
+
+            void Add<T>(Action<TextWriter, T> write)
+                where T : struct
+            {
+                var valueWriter = new ValueWriter<T>(write);
+                SimpleValueWriters.Add(typeof(T), valueWriter);
+                SimpleValueWriters.Add(typeof(T?), valueWriter);
+            }
+        }
 
         public static bool TryGetSimple<T>(T value, out ValueWriter writer)
         {
@@ -20,16 +30,6 @@
         }
 
         public abstract void Write<T>(TextWriter writer, T value);
-
-        private static Dictionary<Type, ValueWriter> CreateWriters(params (Type, ValueWriter)[] writers)
-        {
-            return writers.ToDictionary(tuple => tuple.Item1, tuple => tuple.Item2);
-        }
-
-        private static (Type, ValueWriter) Create<T>(Action<TextWriter, T> write)
-        {
-            return (typeof(T), new ValueWriter<T>(write));
-        }
 
         private static void WriteDouble(TextWriter writer, double value)
         {
