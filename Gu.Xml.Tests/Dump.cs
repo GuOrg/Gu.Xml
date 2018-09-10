1f4707f23e36fe87840c76abb433ecdca0865f7b
Johan Larsson <johan_gavle1@hotmail.com>
2018-09-10 16:45:53 +0200

    Dump.

---
+++ Gu.Xml.Tests/Dump.cs
@@ -0,0 +1,27 @@
+﻿namespace Gu.Xml.Tests
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+
+    public class Dump
+    {
+        [Explicit("Script")]
+        [TestCase(typeof(IEnumerable<>))]
+        [TestCase(typeof(Queue<>))]
+        public void MarkdownList(Type source)
+        {
+            foreach (var type in source.Assembly
+                                       .GetExportedTypes()
+                                       .Where(x => typeof(IEnumerable).IsAssignableFrom(x) &&
+                                                   !x.IsInterface &&
+                                                   !x.IsAbstract &&
+                                                   x.Namespace == source.Namespace))
+            {
+                Console.WriteLine("- [ ] " + type);
+            }
+        }
+    }
+}
