40b5fb7a867d6e4f3d58df1f001fd36b2a3f76fe
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-10 08:11:44 +0200

    Use Expression.Lambda.Compile() instead of CreateDelegate

--- Gu.Xml/Internals/PropertyInfoExt.cs
+++ Gu.Xml/Internals/PropertyInfoExt.cs
@@ -1,35 +1,16 @@
 ﻿namespace Gu.Xml
 {
     using System;
+    using System.Linq.Expressions;
     using System.Reflection;
 
     internal static class PropertyInfoExt
     {
         internal static Func<TSource, TValue> CreateGetter<TSource, TValue>(this PropertyInfo property)
         {
-            if (property.DeclaringType.IsValueType)
-            {
-                return new StructGetter<TSource, TValue>(property).GetValue;
-            }
-
-            return (Func<TSource, TValue>)Delegate.CreateDelegate(typeof(Func<TSource, TValue>), property.GetMethod);
-        }
-
-        private class StructGetter<TSource, TValue>
-        {
-            private readonly GetterDelegate getter;
-
-            public StructGetter(PropertyInfo property)
-            {
-                this.getter = (GetterDelegate)Delegate.CreateDelegate(typeof(GetterDelegate), property.GetMethod, throwOnBindFailure: true);
-            }
-
-            private delegate TValue GetterDelegate(ref TSource source);
-
-            public TValue GetValue(TSource source)
-            {
-                return this.getter(ref source);
-            }
+            var parameter = Expression.Parameter(typeof(TSource));
+            return Expression.Lambda<Func<TSource, TValue>>(Expression.Property(parameter, property), parameter)
+                             .Compile();
         }
     }
 }
