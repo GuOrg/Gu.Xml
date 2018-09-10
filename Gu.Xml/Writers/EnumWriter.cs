ca1ea7967b10958a6d4aae99b0b3654535e1db80
JohanLarsson <johan_gavle1@hotmail.com>
2018-09-10 20:59:06 +0200

    EnumWriter.Integer

--- Gu.Xml/Writers/EnumWriter.cs
+++ Gu.Xml/Writers/EnumWriter.cs
@@ -5,11 +5,26 @@
 
     public sealed class EnumWriter : SimpleValueWriter
     {
-        public static readonly EnumWriter Default = new EnumWriter();
+        private readonly string format;
+
+        /// <summary>
+        /// An <see cref="EnumWriter"/> that serializes with 'G' format string.
+        /// </summary>
+        public static readonly EnumWriter Default = new EnumWriter("G");
+
+        /// <summary>
+        /// An <see cref="EnumWriter"/> that serializes with 'D' format string.
+        /// </summary>
+        public static readonly EnumWriter Integer = new EnumWriter("D");
+
+        private EnumWriter(string format)
+        {
+            this.format = format;
+        }
 
         public override void Write<T>(TextWriter writer, T value)
         {
-            writer.Write(Enum.Format(typeof(T), value, "G").Replace(",", string.Empty));
+            writer.Write(Enum.Format(typeof(T), value, this.format).Replace(",", string.Empty));
         }
     }
 }
